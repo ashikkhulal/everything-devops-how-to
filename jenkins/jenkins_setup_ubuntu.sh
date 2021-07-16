9c7f3d62f66b4d6c48e7cbb30e7e7835ed7e44e3
ashikkhulal <askhulal@gmail.com>
2021-07-15 21:37:45 -0500

    jenkins ubuntu script file updated

--- jenkins/jenkins_setup_ubuntu.sh
+++ jenkins/jenkins_setup_ubuntu.sh
@@ -11,6 +11,4 @@ sudo apt-get update -y
 sudo apt-get install jenkins -y
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
-sudo systemctl start jenkins
-sudo systemctl enable jenkins
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
