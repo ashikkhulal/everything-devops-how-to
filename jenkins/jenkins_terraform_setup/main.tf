bba01e7bb66e0863743e2fd365da96a5e869dda6
ashikkhulal <askhulal@gmail.com>
2021-07-17 18:49:06 -0500

    jenkins ubuntu terraform provisioning file updated

--- jenkins/jenkins_terraform_setup/main.tf
+++ jenkins/jenkins_terraform_setup/main.tf
@@ -39,7 +39,7 @@ resource "azurerm_virtual_network" "vnet" {
 
 # Create a subnet
 resource "azurerm_subnet" "subnet" {
-  name                 = "2021-LEARN-jenkins-snet"
+  name                 = "2021-LEARN-eastus-snet-01"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.0.0/24"]
